4cb6b1acf4bf9b80a1c67615fe54aff3b889c269
Kristine <Kristinematilde@hotmail.no>
2024-01-15 15:49:45 +0100

    app.js

---
+++ app.mjs
@@ -0,0 +1,3 @@
+import chalk from "chalk"
+
+console.log(chalk.blue('Alle') + ' barna' + chalk.red(' så')+ chalk.green(' på')+ chalk.yellow(' mikrobølgeovnen')+ chalk.magenta(' unntatt')+ chalk.redBright(' Minni,')+ chalk.yellowBright(' hun')+ chalk.cyan(' lå')+ chalk.rgb(255,140,0)(' inni'));
